98f745c4c8d7a031859bd5e61ab2c62b732bb59e
Ayodeji Odukoya <ayodeji.od@gmail.com>
2020-10-28 13:26:54 +0000

    string to has conversion

--- main.rb
+++ main.rb
@@ -1,3 +1,24 @@
+class String
+    def to_hash(arr_sep=/\n+/, key_sep='=>', len = 1)
+        array = self.split(arr_sep)
+        hash = {}
+    
+        array.each do |e|
+            key_value = e.split(key_sep)
+            key_value.each { |a| a.strip! if a.respond_to? :strip! }
+            if key_value[0]
+                if (key_value[0].length == len && key_value[1].length == len)
+                    hash[key_value[0]] = key_value[1] 
+                else
+                    raise StandardError.new "Invalid Key/Value"
+                end
+            end
+        end
+
+        return hash
+    end
+end
+
 # Check if variable is a string
 def isString(str)
     #  Raise invalid string exception
