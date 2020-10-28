49db23b1aa289a0221f08b6eb595ef2abf17e81e
Ayodeji Odukoya <ayodeji.od@gmail.com>
2020-10-28 13:06:22 +0000

    main job function

---
+++ main.rb
@@ -0,0 +1,21 @@
+def batchJobs(str)
+    begin
+        isString(str)
+    rescue StandardError => e
+        puts "#{str} #{e.inspect}"
+        return
+    end
+
+    jobs = ''
+
+    return jobs if str.empty?
+
+    begin
+        jobs = jobSorter str.to_hash
+    rescue StandardError => e
+        puts "#{e.inspect}"
+        return
+    end
+
+    jobs.join(', ')
+end
